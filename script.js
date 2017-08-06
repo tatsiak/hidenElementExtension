1bf2135851914fa9859cda0e30008eff24fa1c15
tatsiak <tatsiak@gmail.com>
2017-08-06 15:21:58 +0300

    fixed null value error

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 
 var re = /yapi.(?:[\w\.]+)\/stat\/\?g=(\w+)/g;
 var value = re.exec(document.documentElement.innerHTML);
-if (value[1] != '') {
+if (value != null && value[1] != '') {
     var popup = document.createElement('div');
     var htmlMessage = document.createElement('span');
     popup.style.zIndex = 999;
-    popup.style.width = '280px';
+    popup.style.width = '350px';
     popup.style.height = '20px';
     popup.style.backgroundColor = "#FFFFFF"
     popup.style.position = "absolute";
